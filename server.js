cfcd448b3bdfda0c1280e35ae580d363409e8b69
Manny Romero <Manny.rgarcia98@gmail.com>
2021-01-14 01:24:36 -0500

    organized

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const mongoose = require('mongoose');
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const app = express()
+require('dotenv').config()
 
 // /sessions
 const session = require('express-session')
